136954a166fc101e2bc21aa2090deec587bb3161
yahia200 <yahiaamr90@gmail.com>
2024-04-27 20:47:02 +0300

    CMake

---
+++ build/CMakeFiles/CA.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CA"
+  "CA.pdb"
+  "CMakeFiles/CA.dir/main.c.o"
+  "CMakeFiles/CA.dir/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
